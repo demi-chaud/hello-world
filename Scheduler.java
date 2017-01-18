cce7deeb381ed8131e40eac6bfe4b0f623b61c79
demi-chaud <themichaud@gmail.com>
2017-01-18 16:44:02 -0600

    pre lane-dependent gap acceptance

--- Scheduler.java
+++ Scheduler.java
@@ -4,91 +4,146 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import repast.simphony.context.Context;
+import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.space.continuous.ContinuousSpace;
-import repast.simphony.space.grid.Grid;
 import repast.simphony.util.ContextUtils;
 
+
 /**
- * Class created to run schedule of car creation, etc.
+ * Class created to contain schedule.
+ * Agents are created, they observe their surroundings, then move.
  * @author Darryl Michaud
  */
 public class Scheduler extends Agent {
 	
-	static ArrayList<Turtle> allCars;
-	static ArrayList<Ped> allPeds;
-	Random rndCar = new Random(); //initiates random number generator for Poisson vehicle arrival
-	Random rndPed = new Random(); //ditto for peds so the two are independent
+	static  ArrayList<Turtle> allCars;
+	static  ArrayList<Ped>	 allPeds;
+	static  ArrayList<Turtle> killListC = new ArrayList<Turtle>();
+	static  ArrayList<Ped>	 killListP = new ArrayList<Ped>();
+	Random  rndCar = new Random(); //initiates random number generator for Poisson vehicle arrival
+	Random  rndPed = new Random(); //ditto for peds so the two are independent
+	double  rndC, rndP, rndC2, rndP2, yPlacement;
+	int     lane, dir;
+	boolean carsYes, pedsYes;
 	
-	/**
-	 * Schedules actions outside agents.
-	 * Car creation scheduled as Poisson arrival
+	/*
+	 * The scheduled method that contains all major steps.
 	 */
-	@Override
-	public void step() {
-		//TODO: add second term of poisson PMF to allow for cars arriving in both lanes:
-		//      exp(-lam) * lam^x / x!  will require if statement
-		double lambdaCar = UserPanel.vehRho * RoadBuilder.timeStep / 3600; // vehicle arrivals per tick, currently 0.03
-		double lambdaPed = UserPanel.pedRho * RoadBuilder.timeStep / 3600; // peds per tick, currently 0.003
-		double Pof1Car = lambdaCar * Math.exp(-lambdaCar); // Poisson probability of 1 arrival in a tick (higher counts are negligible)
-		double Pof1Ped = lambdaPed * Math.exp(-lambdaPed); // ditto for peds
-		// From MUTCD 4C.4, traffic higher than 1450 VPM (both ways, w/ PPH > 133) warrants signal
-		//TODO: make cars and peds come from both directions	
-		//TODO: need added buffer to keep cars/peds from being created too close to each other
-		RoadBuilder.ticker += 1;
-		if (UserPanel.carsYes == true) {
-			if (rndCar.nextDouble() <= Pof1Car) {
-				Turtle addedTurtle = addCar();
-				allCars.add(addedTurtle);}}
-		if (UserPanel.pedsYes == true) {
-//			if (RoadBuilder.ticker % 100 == 0) {
-			if (rndPed.nextDouble() <= Pof1Ped) {
-				Ped addedPed = addPed();
-				allPeds.add(addedPed);}}
-		
-		//space holder for breakpoint
-		@SuppressWarnings("unused")
-		int foo = 0; 
+	@ScheduledMethod(start = 1, interval = 1, priority = 1)
+	public void doStuff() {
+		RoadBuilder.flowSource.calc();
+		carsYes = (allCars.size() > 0);
+		pedsYes = (allPeds.size() > 0);
+		if (carsYes) {
+			for (Turtle a : allCars) {
+				a.calc();}}
+		if (pedsYes) {
+			for (Ped a : allPeds) {
+				a.calc();}}
+		if (carsYes) {
+			for (Turtle b : allCars) {
+				b.drive();}}
+		if (pedsYes) {
+			for (Ped b : allPeds) {
+				b.walk();}}
+		if (!killListC.isEmpty()) {
+			for (Turtle c : killListC) {
+				c.die();}
+			killListC = new ArrayList<Turtle>();}
+		if (!killListP.isEmpty()) {
+			for (Ped c : killListP) {
+				c.die();}
+			killListP = new ArrayList<Ped>();}
+		RoadBuilder.ticker ++;
+	}
+	
+	public void calc() {
+		if (UserPanel.vehRho > 0) {
+			rndC = rndCar.nextDouble();
+			if (rndC <=  UserPanel.Pof2Car) {
+				Turtle addedTurtle1 = addCar(0,1);
+				Turtle addedTurtle2 = addCar(1,1);
+				allCars.add(addedTurtle1);
+				allCars.add(addedTurtle2);}
+			else if (rndC <= UserPanel.Pof1Car) {
+				lane = rndCar.nextInt(2);		// picks between 0 and 1 randomly
+				Turtle addedTurtle = addCar(lane,1);
+				allCars.add(addedTurtle);}
+			if (UserPanel.bothCar == true) {
+				rndC2 = rndCar.nextDouble();
+				if (rndC2 <=  UserPanel.Pof2Car) {
+					Turtle addedTurtle1 = addCar(0,-1);
+					Turtle addedTurtle2 = addCar(1,-1);
+					allCars.add(addedTurtle1);
+					allCars.add(addedTurtle2);}
+				else if (rndC2 <= UserPanel.Pof1Car) {
+					lane = rndCar.nextInt(2);		// picks between 0 and 1 randomly
+					Turtle addedTurtle = addCar(lane,-1);
+					allCars.add(addedTurtle);}}}
+		if (UserPanel.pedRho > 0) {
+			if (UserPanel.pedsUp == true) {
+				rndP = rndPed.nextDouble();
+//				if (RoadBuilder.ticker % 200 == 0) { // Constant stream for testing
+				if (rndP <=  UserPanel.Pof2Ped) {
+					Ped addedPed1 = addPed(1);
+					Ped addedPed2 = addPed(1);
+					allPeds.add(addedPed1);
+					allPeds.add(addedPed2);}
+				else if (rndP <= UserPanel.Pof1Ped) {
+					Ped addedPed = addPed(1);
+					allPeds.add(addedPed);}}
+			if (UserPanel.pedsDn == true) {
+				rndP2 = rndPed.nextDouble();
+				if (rndP2 <=  UserPanel.Pof2Ped) {
+					Ped addedPed1 = addPed(-1);
+					Ped addedPed2 = addPed(-1);
+					allPeds.add(addedPed1);
+					allPeds.add(addedPed2);}
+				else if (rndP2 <= UserPanel.Pof1Ped) {
+					Ped addedPed = addPed(-1);
+					allPeds.add(addedPed);}}}
 	}
 	
 	/**
-	 * Used to add cars to left edge
+	 * Used to add cars to edge
 	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Turtle addCar() {
-		Context context = ContextUtils.getContext(this);
+	@SuppressWarnings("unchecked")
+	public Turtle addCar(int lane, int dir) {
+		Context<Object> context = ContextUtils.getContext(this);
 		ContinuousSpace<Object> space = (ContinuousSpace<Object>) context.getProjection("space");
-		Grid<Object> grid = (Grid) context.getProjection("grid");
+//		Grid<Object> grid = (Grid) context.getProjection("grid");		
 		
-		int lane = rndCar.nextInt(2); // picks between 0 and 1 randomly
-		double yPlacement = (double)lane * (RoadBuilder.roadW/2) + (RoadBuilder.roadW/4);
-		
-		Turtle newTurtle = new Turtle(space,grid);
+//		Turtle newTurtle = new Turtle(space,grid);
+		Turtle newTurtle = new Turtle(space,lane,dir);
 		context.add(newTurtle);
-		space.moveTo(newTurtle,0,yPlacement);
+		if (dir == 1) {				//1 = going right, -1 = going left
+			yPlacement = RoadBuilder.sidewalk + (double)lane * (RoadBuilder.laneW) + (RoadBuilder.laneW/2);
+			space.moveTo(newTurtle,0,yPlacement);}
+		else {
+			yPlacement = RoadBuilder.sidewalk + RoadBuilder.roadW - (double)lane * (RoadBuilder.laneW) - (RoadBuilder.laneW/2);
+			space.moveTo(newTurtle,RoadBuilder.roadL - 1,yPlacement);}
 		return(newTurtle);
 	}
 	
 	/**
 	 * Used to add peds at x-walk
+	 * @param direction: 1 walks up, -1 walks down
 	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Ped addPed() {
-		Context context = ContextUtils.getContext(this);
+	@SuppressWarnings("unchecked")
+	public Ped addPed(int direction) {
+		Context<Object> context = ContextUtils.getContext(this);
 		ContinuousSpace<Object> space = (ContinuousSpace<Object>) context.getProjection("space");
-		Grid<Object> grid = (Grid) context.getProjection("grid");
+//		Grid<Object> grid = (Grid) context.getProjection("grid");
+//		int dir0 = rndPed.nextInt(2); //picks between 0 (walking up) and 1 (walking down)
+		double dir0 = direction == 1 ? 0 : 1;
+		double yPlacement = dir0 * RoadBuilder.worldW;
+		if (direction == -1) yPlacement -= 1e-15;
 		
-		/* TODO: add peds coming from both directions
-		int dir0 = rndPed.nextInt(2); //picks between 0 (walking up) and 1 (walking down)
-		int dir = dir0 == 0 ? 1 : -1;
-		double yPlacement = (double)dir0 * RoadBuilder.worldW;
-		*/
-		
-		int dir = 1;
-		double yPlacement = 0;
-		
-		Ped newPed = new Ped(space,grid,dir);
+//		Ped newPed = new Ped(space,grid,direction);
+		Ped newPed = new Ped(space,direction);
 		context.add(newPed);
 		space.moveTo(newPed,RoadBuilder.xWalkx,yPlacement);
+		newPed.myLoc = space.getLocation(newPed);
 		return(newPed);
 	}
 	
--- Turtle.java
+++ Turtle.java
@@ -1,11 +1,12 @@
 package driving1;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+import repast.simphony.parameter.Parameter;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.NdPoint;
-import repast.simphony.space.grid.Grid;
 
 /**
 * Vehicle agent class. Defines creation and actions.
@@ -15,71 +16,152 @@ import repast.simphony.space.grid.Grid;
 
 public class Turtle extends Agent{
 	ContinuousSpace<Object> space;
-	Grid<Object> grid;
-	Random rnd = new Random(); //initiates random number generator for vehicle properties
-	public double v, acc, xLoc;
-	double tGap, jamHead, maxv, mina, maxa;
-	public int lane; // 0 -> bottom lane, 1 -> top lane
-	//double vision;
+//	Grid<Object> grid;
+	private ArrayList<Turtle> sameDir, ahead, leaders, behind, followers;
+	public  Turtle leader, follower;
+	private List<double[]> storage = new ArrayList<double[]>();
+	private Random  rnd = new Random(); 			//initiates random number generator for vehicle properties
+	public  double	v, vNew, acc, xLoc;
+	private double	tGap, jamHead, maxv, mina, maxa, newAcc, tail, wS, etaS, wV, etaV, sigR;
+	private double	delayTs = UserPanel.delayTs;
+	private double	stamp, tStamp, delayedT, hiT;
+	public  int		age, lane; 				// lane = 0 -> outer lane,   1 -> inner lane
+	public	int		dir;					// dir  = 1 -> going right, -1 -> going left
+	private int		thisLane, thisDir;
+	private boolean	distracted = false; 		// 1 = yes, figure out how to implement this
+	private NdPoint	myLoc;
+	private double	tN = Math.floor(delayTs/UserPanel.tStep);
+	private double	tBeta = (delayTs/UserPanel.tStep) - tN;
+//	double vision;
 	
 	/**
-	* Used to move cars at each tick.
+	* High-level method of driver action.
 	*/
-	@Override
-	public void step() {
-		double vNew;
-		NdPoint myLoc = space.getLocation(this);
-		this.xLoc = myLoc.getX();
-		int thisLane = lane;
+	public void calc() {
+		myLoc = space.getLocation(this);
+		xLoc = myLoc.getX();
+		thisLane = lane;
+		thisDir  = dir;
 		
-		acc = accel(myLoc, thisLane);
+		if (distracted == false) newAcc = accel(myLoc, thisLane, thisDir);
+		else newAcc = acc;
+		
+		//delayed reaction implements acc calculated and stored delayT ago
+		if (UserPanel.delayTs > 0) {
+			stamp  = RoadBuilder.clock.getTickCount();
+			tStamp = stamp*UserPanel.tStep;
+			//TODO: limit size of storage to limit memory use
+			storage.add(new double[] {tStamp, newAcc, v});
+			delayedT = tStamp - delayTs;
+			hiT = 0;
+			int foo = 0;
+			if (storage.size() > 3 && storage.size() > tN+1) { 
+				while (hiT < delayedT) {
+					hiT = storage.get(foo)[0];
+					foo++;}
+				double hiAcc = storage.get(foo-1)[1];
+				if (hiT != delayedT){	//linear interpolation (is there a better approx?)
+					double loAcc = storage.get(foo-2)[1];
+					acc = tBeta*loAcc + (1-tBeta)*hiAcc;}
+				else acc = hiAcc;}
+			else acc = newAcc;
+			age ++;}
+		else acc = newAcc;
+		
 		vNew = v + acc;
 		if (vNew < 0) {vNew = 0;}
 		if (vNew > maxv) {vNew = maxv;}
-		
-		// Die or move?
-		if (xLoc + vNew >= RoadBuilder.roadL) {
-			die();}
-		else if (vNew != 0) {
-			space.moveByDisplacement(this,vNew,0);
-			myLoc = space.getLocation(this);
-			xLoc = myLoc.getX();
-			grid.moveTo(this,(int)myLoc.getX(),(int)myLoc.getY());}
-		v = vNew;}
+	}		
 
+	public void drive() {
+		if (dir == 1) {
+			if (xLoc + vNew >= RoadBuilder.roadL - 1) {
+				Scheduler.killListC.add(this);}
+			else if (vNew != 0) {
+				space.moveByDisplacement(this,vNew,0);
+				myLoc = space.getLocation(this);
+				xLoc = myLoc.getX();}}
+		else {
+			if (xLoc - vNew <= 0) {
+				Scheduler.killListC.add(this);}
+			else if (vNew != 0) {
+				space.moveByDisplacement(this,-vNew,0);
+				myLoc = space.getLocation(this);
+				xLoc = myLoc.getX();}}
+		v = vNew;
+	}
+//	grid.moveTo(this,(int)myLoc.getX(),(int)myLoc.getY());
+	
 	/**
 	 * Used to calculate car-following behavior based on location.
 	 */
-	public double accel(NdPoint loc, int myLane) {
-		double setSpeed = maxv;
+	public double accel(NdPoint loc, int myLane, int myDir) {
 		double thisX = loc.getX();
-		double a;
-		ArrayList<Turtle> ahead = new ArrayList<Turtle>();
-		ArrayList<Turtle> leaders = new ArrayList<Turtle>();
-		Turtle leader = null;
-		double head = RoadBuilder.roadL;
-		for (Turtle m : Scheduler.allCars) {
-			if (m.xLoc > thisX) {
-				ahead.add(m);}}
+		double a, head, setSpeed, vDiff, safeHead;
+		sameDir = new ArrayList<Turtle>();
+		ahead	= new ArrayList<Turtle>();
+		leaders	= new ArrayList<Turtle>();
+		head	= RoadBuilder.roadL;
+		//TODO: this if loop will go away if lane-changes are included
+		if (follower == null) {
+			behind	  = new ArrayList<Turtle>();
+			followers = new ArrayList<Turtle>();
+			leader	  = null;
+			tail	  = RoadBuilder.roadL;}
+		for (Turtle p : Scheduler.allCars) {
+			if (p.dir == myDir) {
+				sameDir.add(p);}}
+		if (!sameDir.isEmpty()) {
+			for (Turtle m : sameDir) {
+				if (myDir == 1) {					if (m.xLoc > thisX) {						ahead.add(m);}
+					if (follower == null) {
+						if (m.xLoc < thisX) {
+							behind.add(m);}}}
+				else {
+					if (m.xLoc < thisX) {
+						ahead.add(m);}
+					if (follower == null) {
+						if (m.xLoc > thisX) {
+							behind.add(m);}}}}}
 		if (!ahead.isEmpty()) {
 			for (Turtle n : ahead) {
 				if (n.lane == myLane) {
-					leaders.add(n);}}
-			if(!leaders.isEmpty()) {
-				for (Turtle o : leaders) {
-					if(o.xLoc - thisX < head) {
-						head = o.xLoc - thisX;
-						leader = o;}}}}
+					leaders.add(n);}}}
+		if (follower == null) {
+			if (!behind.isEmpty()) {
+				for (Turtle n : behind) {
+					if (n.lane == myLane) {
+						followers.add(n);}}}}
+		if (!leaders.isEmpty()) {
+			for (Turtle o : leaders) {
+				if(Math.abs(o.xLoc - thisX) < head) {
+					head = Math.abs(o.xLoc - thisX); //TODO: adjust this to include car length
+					leader = o;}}}
+		if (follower == null) {
+			if (!followers.isEmpty()) {
+				for (Turtle o : followers) {
+					if(Math.abs(o.xLoc - thisX) < tail) {
+						tail = Math.abs(o.xLoc - thisX); //TODO: adjust this to include car length
+						follower = o;}}}}
+		else {
+			tail = Math.abs(follower.xLoc - thisX);}
 		if (leader != null) {
-			setSpeed = leader.v;
-			double vDiff = setSpeed - v;
-			//double check the variables here - is setSpeed supposed to be used again?
-			double safeHead = (jamHead + v*(tGap/RoadBuilder.timeStep) +
+			if (UserPanel.estErr == true) {   // includes estimation error in headway measuring
+				etaS = rnd.nextGaussian();
+				etaV = rnd.nextGaussian();
+				wS = UserPanel.wien1*wS + UserPanel.wien2*etaS;
+				wV = UserPanel.wien1*wV + UserPanel.wien2*etaV;
+				head = head*Math.exp(UserPanel.Vs*wS);
+				setSpeed = leader.v - head*sigR*wV;}
+			else setSpeed = leader.v;
+			vDiff = setSpeed - v;
+			safeHead = (jamHead + v*(tGap) +
 					((v*vDiff)/(2*Math.sqrt(maxa*mina))));
 			a = maxa*(1 - Math.pow(v/maxv,4) - Math.pow(safeHead/head,2));}
 		else {a = maxa*(1 - Math.pow(v/maxv,4));}
 		
-		return a;}
+		return a;
+	}
 
 	/**
 	 * Creates vehicle agents and initializes values
@@ -88,16 +170,26 @@ public class Turtle extends Agent{
 	 * @param contextGrid
 	 */
 	//TODO: add similar code to Ped.java to vary ped parameters
-	public Turtle(ContinuousSpace<Object> contextSpace, Grid<Object> contextGrid) {
+//	public Turtle(ContinuousSpace<Object> contextSpace, Grid<Object> contextGrid) {
+	public Turtle(ContinuousSpace<Object> contextSpace, int whichLane, int whichDir) {	
 		space = contextSpace;
-		grid  = contextGrid;
-		maxa = rnd.nextGaussian()*(RoadBuilder.maxa*.08)+RoadBuilder.maxa;
-		mina = rnd.nextGaussian()*(RoadBuilder.mina*.08)+RoadBuilder.mina;
-		maxv = rnd.nextGaussian()*(.2*RoadBuilder.vLimit/RoadBuilder.vBase)+(RoadBuilder.vLimit/RoadBuilder.vBase);
-		// stdDev of maxv is stretched to .2*mean to demonstrate car-following behavior
-		tGap = rnd.nextGaussian()*(RoadBuilder.tGap*.08)+RoadBuilder.tGap;
-		jamHead = rnd.nextGaussian()*(RoadBuilder.jamHead*.08)+RoadBuilder.jamHead;
-		v = maxv * (1 - .3*rnd.nextDouble());}
+//		grid  = contextGrid;
+		lane  = whichLane;
+		dir   = whichDir;
+		//store parameters with heterogeneity (currently s.dev = 8% of mean)
+		//TODO: get theory for these numbers
+		maxa = rnd.nextGaussian()*(UserPanel.maxa*.08)+UserPanel.maxa;
+		mina = rnd.nextGaussian()*(UserPanel.mina*.08)+UserPanel.mina;
+		maxv = rnd.nextGaussian()*(.2*UserPanel.sLimit)+(UserPanel.sLimit);
+		// TODO: stdDev of maxv is stretched to .2*mean to demonstrate car-following behavior
+		tGap = rnd.nextGaussian()*(UserPanel.tGap*.08)+UserPanel.tGap;
+		jamHead = rnd.nextGaussian()*(UserPanel.jamHead*.08)+UserPanel.jamHead;
+		v = maxv * (1 - .3*rnd.nextDouble());
+		age = 0;
+		wS = etaS = rnd.nextGaussian();
+		wV = etaV = rnd.nextGaussian();
+		sigR = 0.01; //standard deviation of relative approach rate TODO: should this vary?
+	}
 	
 	/**
 	 * Getter for identification
@@ -107,11 +199,30 @@ public class Turtle extends Agent{
 		return 1;}
 	
 	/**
-	 * Code for psycho-spatial car following behavior
-	 * will be called in step after IDM script is moved to separate method
-	 * AX = measured from rear bumper to 
-	 * AX
+	 * Parameter declarations for probe
 	 */
+	@Parameter(usageName="newAcc", displayName="Calculated acc")
+	public double getAcc() {
+		return newAcc;}
+	@Parameter(usageName="v", displayName="Current vel")
+	public double getVel() {
+		System.out.println(leader);
+		return v;}
+	@Parameter(usageName="lane", displayName="Current lane")
+	public double getLane() {
+		return lane;}
+	@Parameter(usageName="leader", displayName="Current leader")
+	public Turtle getLead() {
+		return leader;}
+}
+
+
+//	/**
+//	 * Code for psycho-spatial car following behavior
+//	 * would be called in step() if IDM script is moved to separate method
+//	 * AX = measured from rear bumper to 
+//	 * AX
+//	 */
 //	public Psycho() {
 //		/**
 //		 * all headways measured from front of lead veh to front of following veh (incl. length of lead veh)
@@ -123,11 +234,5 @@ public class Turtle extends Agent{
 //		double SdelX = AX + BX*Math.sqrt(this.v)*EX; //maximum following distance threshold
 //	
 //		double CX; //constant related to threshold of perception for visual angle divergence
-//		double delX; //headway TODO: change name
-//		double CLdelV = -Math.pow(delX,2)/Math.pow(CX,2);
-//		
-//	}
-	
-	
-	
-}
+//		double delX; //headway (needs name change)
+//		double CLdelV = -Math.pow(delX,2)/Math.pow(CX,2);}
