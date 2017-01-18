0afd9bf228ae96a8d4561b165d1bf2e36ba7d2a7
demi-chaud <themichaud@gmail.com>
2017-01-18 16:55:51 -0600

    pre lane-dependent gap acceptance (#4)

--- Agent.java
+++ Agent.java
@@ -2,36 +2,19 @@ package driving1;
 
 import repast.simphony.annotate.AgentAnnot;
 import repast.simphony.context.Context;
-import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.util.ContextUtils;
 
 /**
- * General agent class to handle creation, scheduling, and bookkeeping
+ * General agent class for bookkeeping
  * @author Darryl Michaud
  */
 
 @AgentAnnot(displayName = "Agent")
 public class Agent {
 	
-	// Initialize variables
-	//Parameters params = RunEnvironment.getInstance().getParameters();
-	//double vehRho = (double)params.getValue("veh_vol");
-	//double pedRho = (double)params.getValue("ped_vol");
-	//double vLimit = (double)params.getValue("v_limit");
-	//double timeStep = (double)params.getValue("time_step");
-	//TODO: put these within step() (will need to change to @extend) to allow updates on the fly
-	
-	/**
-	 * Method to schedule agent actions, details are overridden by subclasses
-	 */
-	@ScheduledMethod(start = 1, interval = 1)
-	public void step() {
-		//overridden
-	}
-	
     /**
      * Eliminates agents (called when agents move outside the space)
-	*/
+	 */
 	@SuppressWarnings("rawtypes")
 	public void die(){
 		if (this.isCar() == 1) {
@@ -39,7 +22,8 @@ public class Agent {
 		else if (this.isPed() == 1) {
 			Scheduler.allPeds.remove(this);}
 		Context context = ContextUtils.getContext(this);
-		context.remove(this);}
+		context.remove(this);
+	}
 
 	/**
 	 * Bookkeeping methods
@@ -49,8 +33,4 @@ public class Agent {
 		return 0;}
 	public int isPed() {
 		return 0;}
-	//public double getV() {
-	//	return v;}
-	//public void setV(double vel) {
-	//	this.v = vel;}
 }
--- Ped.java
+++ Ped.java
@@ -1,67 +1,205 @@
 package driving1;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import org.apache.commons.math3.util.FastMath;
+
+import repast.simphony.parameter.Parameter;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.NdPoint;
-import repast.simphony.space.grid.Grid;
 
 /**
- * Pedestrian agent class - currently peds do not interact with each other
- * @author yawei
+ * Pedestrian agent class
+ * @author Darryl Michaud
  */
-
 public class Ped extends Agent{
-	private ContinuousSpace<Object> space;
-	private Grid<Object> grid;
-	public double v;
-	private double xLoc, yLoc, maxV;
-	public int dir;  // 1 walks up, -1 walks down
-	public int crossing; // 0=not yet, 1=waiting, 2=yes, 3=done
+	private	ContinuousSpace<Object> space;
+//	private	Grid<Object> grid;
+	public  NdPoint myLoc;
+	private	NdPoint endPt;
+	private boolean go, curbed;
+	public  double[] v, dv, newV, frogger;
+	public  double[] zero = new double[] {0,0};
+	private	double xLoc, yLoc, maxV, gap, endPtDist, endPtTheta;
+	private double side = RoadBuilder.sidewalk;
+	private Random rnd = new Random();
+	private double wS, etaS, wV, etaV, sigR;
+	public	int age, dir;	// dir = 1 walks up, -1 walks down
+	public	int crossing;	// 0=not yet, 1=waiting, 2=yes, 3=done
+	public	Turtle nearest;
+
 	
-	//TODO: revisit this
-	//double bubble = 0.5 / RoadBuilder.spaceScale; //gives .5m gap between peds at rest 
+	//3-circle variables - from Helbing, et al (2000)
+	private double accT  = 0.5/UserPanel.tStep;							//acceleration time
+	private double m     = 80;											//avg ped mass in kg
+	private double horiz = 5/RoadBuilder.spaceScale;					//distance at which peds affect each other
+	private double A     = 2000*UserPanel.tStep/RoadBuilder.spaceScale;	//ped interaction constant (kg*space units/time units)
+	private double B     = 0.08/RoadBuilder.spaceScale;					//ped distance interaction constant (space units)
+	public  double r     = 0.6/RoadBuilder.spaceScale;					//ped radius (space units)
+	private double k	 = 120000*UserPanel.tStep;						//wall force constant
+	//TODO: figure out A & k wrt one or two factors of time (currently one like acceleration)
 	
 	//TODO: inroad() script/flag
-	@Override 
-	public void step() {
-		NdPoint myLoc = space.getLocation(this);
-		xLoc = myLoc.getX();
-		yLoc = myLoc.getY();
-		double disp = dir*v;
-		double side = RoadBuilder.sidewalk;
+	
+	public void calc() {
+		myLoc = space.getLocation(this);
+		xLoc  = myLoc.getX();
+		yLoc  = myLoc.getY();
+		dv    = accel(myLoc,dir,curbed);
+		newV  = sumV(v,dv);
 		
 		switch (crossing) {
 		case 0: if (dir == 1) {
-					if (yLoc + disp >= side) {
-						space.moveTo(this,xLoc,side);
-						myLoc = space.getLocation(this);
-						grid.moveTo(this,(int)myLoc.getX(),(int)myLoc.getY());
-						this.v = 0;
-						crossing = 1;}
-					else move(myLoc,disp);}
+					if (yLoc + newV[1] >= side - r) {
+						curbed = true;
+						dv = accel(myLoc,dir,curbed);
+						newV = sumV(v,dv);}}
 				else {
-					if (yLoc + disp <= side + RoadBuilder.roadW) {
-						space.moveTo(this,xLoc, side+RoadBuilder.roadW);
-						myLoc = space.getLocation(this);
-						grid.moveTo(this,(int)myLoc.getX(),(int)myLoc.getY());
-						crossing = 1;}
-					else move(myLoc,disp);}
+					if (yLoc + newV[1] <= side + RoadBuilder.roadW + r) {
+						curbed = true;
+						dv = accel(myLoc,dir,curbed);
+						newV = sumV(v,dv);}}
 				break;
-		case 1: disp = yield();
-				this.v = disp;
-				if (disp != 0) move(myLoc,disp);
+		case 1: dv = yield();
+				newV = sumV(v,dv);
+				break;		
+		default: break;}
+		
+		if (this.dir == 1) {
+			if (newV[1] < 0) {
+				newV[1] = 0;}}
+		else {
+			if (newV[1] > 0) {
+				newV[1] = 0;}}
+		
+		double totalV = Math.sqrt(newV[0]*newV[0] + newV[1]*newV[1]);
+		if (totalV > maxV) {
+			double norm = maxV/totalV;
+			newV[0] = newV[0]*norm;
+			newV[1] = newV[1]*norm;}
+	}
+	
+	
+	
+	/*
+	 * Scheduled movement method
+	 */
+	public void walk() {
+		v = newV;
+		move(myLoc,v);
+		yLoc = myLoc.getY();
+		switch (crossing) {
+		case 0:	if (dir == 1) {
+					if (side - yLoc < r || v[1] == 0) {
+						curbed = true;
+						crossing = 1;}}
+				else {
+					if (yLoc - RoadBuilder.roadW - side < r || v[1] == 0) {
+						curbed = true;
+						crossing = 1;}}
 				break;
-		case 2:	move (myLoc,disp);
-				myLoc = space.getLocation(this);
-				yLoc = myLoc.getY();
-				if (dir == 1) {
+		case 2: if (dir == 1) {
 					if (yLoc >= side + RoadBuilder.roadW) crossing = 3;}
-				else if (yLoc <= side) {crossing = 3;}
-				break;
-		case 3: move(myLoc,disp);
+				else {
+					if (yLoc <= side) crossing = 3;}
 				break;
 		default: break;}
+		age ++;
+	}
+	
+	
+	public double[] accel(NdPoint location, int direct, boolean curb) {
+		List<Double>  forcesX = new ArrayList<Double>();
+		List<Double>  forcesY = new ArrayList<Double>();
+		double xF = 0;
+		double yF = 0;
+		double[] acc;
+		
+		//calculate heading to endpoint
+		endPtDist  = space.getDistance(location, endPt); 
+		double endPtDelX  = endPt.getX()-location.getX();
+		endPtTheta = FastMath.asin((double)direct*endPtDelX/endPtDist);
+		if (direct == -1) {
+			endPtTheta += Math.PI;}
+		
+		//calculate motive force
+		Double motFx = (maxV*Math.sin(endPtTheta) - v[0])/accT;
+		Double motFy = (maxV*Math.cos(endPtTheta) - v[1])/accT;
+		forcesX.add(motFx);
+		forcesY.add(motFy);
+		
+		//TODO: add walls to the ped waiting area, maybe add line to make ppl walk if those near them decide to
+		
+		//calculate interactive forces
+		//TODO: write code to make a threshold for interaction instead of the arbitrary horizon
+		for (Ped a : Scheduler.allPeds) {
+			if (a != this) {
+				NdPoint otherLoc = space.getLocation(a);
+				double absDist = space.getDistance(location, otherLoc);
+				if (absDist < horiz) {
+					double delX    = location.getX()-otherLoc.getX();
+					double delY    = location.getY()-otherLoc.getY();
+					double delXabs = Math.abs(delX);
+					double signFx  = Math.signum(delX);
+					double signFy  = Math.signum(delY);
+					double theta   = FastMath.asin(delXabs/absDist);
+					double rij     = r + a.r;
+					Double interFx = signFx*A*Math.exp((rij-absDist)/B)*Math.sin(theta)/m;
+					Double interFy = signFy*A*Math.exp((rij-absDist)/B)*Math.cos(theta)/m;
+					forcesX.add(interFx);
+					forcesY.add(interFy);}}}
+		
+		//stop at curb if necessary
+		if (curb == true) {
+			Double curbF;
+			if (direct == 1) {
+				double dCurb = side - yLoc;
+				curbF = -(A*Math.exp((r-dCurb)/B) + k*(r-dCurb))/m;}
+			else {
+				double dCurb = yLoc - side - RoadBuilder.roadW;
+				curbF = (A*Math.exp((r-dCurb)/B) + k*(r-dCurb))/m;}
+			forcesY.add(curbF);}
+
+		//sum all forces
+		for (Double b : forcesX) {
+			xF += b;}
+		for (Double c : forcesY) {
+			yF += c;}
+		acc = new double[] {xF, yF}; 
+		return acc;
+	}
+
+	
+	
+	/**
+	 * Moves peds and updates context
+	 * @param loc
+	 * overloaded. optional parameters:
+	 * @param displacement
+	 * @param destination
+	 */
+	public void move(NdPoint loc, double[] displacement) {
+		double yl = loc.getY();
+		if (yl + displacement[1] > RoadBuilder.worldW || yl + displacement[1] < 0) {
+			Scheduler.killListP.add(this);}
+		else if (displacement != zero) {	
+			space.moveByDisplacement(this,displacement);
+			myLoc = space.getLocation(this);
+//			grid.moveTo(this,(int)myLoc.getX(),(int)myLoc.getY());
+		}
+	}
+	public void move(NdPoint loc, NdPoint destination) {
+		double xd = destination.getX();
+		double yd = destination.getY();
+		if (yd > RoadBuilder.worldW || yd < 0) {
+			Scheduler.killListP.add(this);}
+		else if (loc != destination) {
+				space.moveTo(this,xd,yd);
+				myLoc = space.getLocation(this);
+//				grid.moveTo(this,(int)myLoc.getX(),(int)myLoc.getY());
+		}
 	}
 	
 	/**
@@ -69,21 +207,24 @@ public class Ped extends Agent{
 	 * TODO: Currently only works for direction = 1
 	 * TODO: will need fundamental rewriting to take double threat into account
 	 * TODO: need to add cars coming from other direction
-	 * TODO: still needs calc of car position in time
 	 */
 	
-	@SuppressWarnings("unused")
-	public double yield() {
-		double frogger;
-		int direction = dir; 
+	public double[] yield() {
 		ArrayList<Turtle> approaching = new ArrayList<Turtle>();
 //		ArrayList<Turtle> approaching0 = new ArrayList<Turtle>();
 //		ArrayList<Turtle> approaching1 = new ArrayList<Turtle>();
+//		ArrayList<Turtle> approaching2 = new ArrayList<Turtle>();
+//		ArrayList<Turtle> approaching3 = new ArrayList<Turtle>();		
 //		ArrayList<Turtle> near = new ArrayList<Turtle>();
-		Turtle nearest = null;
-		double gap = RoadBuilder.roadL/2;
+		//TODO: add double threat using theta to oncoming cars. if within range of same value, not seen
+		
+		nearest = null;
+		xLoc = myLoc.getX();
+		gap = RoadBuilder.roadL/2;
 		for (Turtle m : Scheduler.allCars) {
-			if (m.xLoc < RoadBuilder.xWalkx) {
+			double thisGap = xLoc - m.xLoc;
+			int threat = m.dir * (int)Math.signum(thisGap);
+			if (threat == 1) {
 				approaching.add(m);}}
 		if (!approaching.isEmpty()) {
 //			for (Turtle n : approaching) {
@@ -93,34 +234,67 @@ public class Ped extends Agent{
 //					approaching1.add(n);}}
 //			if(!approaching0.isEmpty()) {
 				for (Turtle o : approaching) {
-					if(RoadBuilder.xWalkx - o.xLoc < gap) {
-						gap = RoadBuilder.xWalkx - o.xLoc;
+					double thisGap = Math.abs(xLoc - o.xLoc);
+					if(thisGap < gap) {
+						gap = thisGap;
 						nearest = o;}}}
 		if (nearest != null) {
-			frogger = 0;}
+			go = gap(nearest);
+			if (go==true) {
+				curbed = false;
+				crossing = 2;}
+			}
 		else {
-			frogger = this.dir * this.maxV;
-			this.crossing = 2;}
+			curbed = false;
+			crossing = 2;}
+		frogger = accel(myLoc,dir,curbed);
 		return frogger;
 	}
 	
-	/**
-	 * Moves peds and updates context
-	 * @param loc
-	 * @param displacement
-	 */
-	
-	public void move(NdPoint loc, double displacement) {
-		double yl = loc.getY();
-		if (yl + displacement > RoadBuilder.worldW || yl + displacement < 0) {
-			die();}
-		else if (displacement != 0) {
-				space.moveByDisplacement(this,0,displacement);
-				loc = space.getLocation(this);
-				grid.moveTo(this,(int)loc.getX(),(int)loc.getY());}
+	//TODO: include estimation errors
+	public boolean gap(Turtle t) {
+		go = false;
+//		int approachL;
+		double approachV, approachX, dist, TTC;
+		//TODO: xTime can be calculated more precisely (compare to HCM eq 18-17)
+		//TODO: make xTime lane-dependent for some peds
+		double xTime = accT + (endPtDist - side) / Math.abs(maxV*Math.cos(endPtTheta)); 
+		sigR = 0.01*UserPanel.tStep; //standard deviation of relative approach rate TODO: should this vary?
+		
+		
+		approachX = t.xLoc;
+		dist	  = Math.abs(xLoc - approachX); 
+		approachV = t.v;
+//		approachL = t.lane;
+		
+		if (UserPanel.estErr == true) {
+			etaS = rnd.nextGaussian();
+			etaV = rnd.nextGaussian();
+			wS = UserPanel.wien1*wS + UserPanel.wien2*etaS;
+			wV = UserPanel.wien1*wV + UserPanel.wien2*etaV;
+			approachV = t.v - dist*sigR*wV;
+			dist = dist*Math.exp(UserPanel.Vs*wS);}
+		
+		TTC  = dist/approachV;
+		if (xTime < TTC) go = true;
+		
+		return go;
 	}
 	
-	//TODO: give x-walk width, distribute peds, add interaction
+	/**
+	 * Adds 2-dimensional arrays
+	 * @param  a = double[2]
+	 * @param  b = double[2]
+	 * @return c = double[2]
+	 */
+	public double[] sumV(double[] a, double[] b) {
+		double[] c = new double[2];
+		for (int i = 0; i < 2; i++) {
+			c[i] = a[i] + b[i];}
+		return c;
+	}
+	
+	//TODO: give x-walk width, distribute peds
 	
 	/**
 	 * Creates pedestrian agents and initializes values
@@ -129,14 +303,21 @@ public class Ped extends Agent{
 	 * @param contextGrid
 	 * @param direction
 	 */
-	public Ped(ContinuousSpace<Object> contextSpace, Grid<Object> contextGrid, int direction) {
+//	public Ped(ContinuousSpace<Object> contextSpace, Grid<Object> contextGrid, int direction) {
+	public Ped(ContinuousSpace<Object> contextSpace, int direction) {
 		space = contextSpace;
-		grid  = contextGrid;
-		//v     = RoadBuilder.pedVavg * 1000 / 3600;
-		maxV  = RoadBuilder.pedVavg / RoadBuilder.vBase;
-		v     = maxV;
+//		grid  = contextGrid;
+//		v     = RoadBuilder.pedVavg * 1000 / 3600;
+		maxV  = UserPanel.pedVavg;
 		dir   = direction; // 1 moves up, -1 moves down
+		v     = new double[] {0,(double)dir*.5*maxV};
+		if (dir == 1) endPt = new NdPoint(RoadBuilder.xWalkx + 2/RoadBuilder.spaceScale, RoadBuilder.worldW);
+		else endPt = new NdPoint(RoadBuilder.xWalkx - 2/RoadBuilder.spaceScale, 0);
 		crossing = 0;
+		curbed   = false;
+		wS = etaS = rnd.nextGaussian();
+		wV = etaV = rnd.nextGaussian();
+		age = 0;
 	}
 	
 	/**
@@ -145,45 +326,15 @@ public class Ped extends Agent{
 	@Override
 	public int isPed() {
 		return 1;}
+	
+	/**
+	 * Parameter declarations for probe
+	 */
+	@Parameter(usageName="v", displayName="Current vel")
+	public double[] getVel() {
+		System.out.println(nearest);
+		return v;}
+	@Parameter(usageName="crossing", displayName="Crossing?")
+	public double getCrossing() {
+		return crossing;}
 }
-
-
-//
-///**
-// * Attempt at using car-follwing to match ped speeds - probably not good approach
-// */
-//public double crowd(NdPoint loc, int myDir) {
-//	double setSpeed = this.maxV;
-//	double thisY = loc.getY();
-//	ArrayList<Ped> ahead = new ArrayList<Ped>();
-//	ArrayList<Ped> leaders = new ArrayList<Ped>();
-//	Ped leader = null;
-//	double head = RoadBuilder.worldW;
-//	if (this.dir == 1) {
-//		for (Ped m : Scheduler.allPeds) {
-//			if (m.yLoc > thisY) {
-//				ahead.add(m);}}}
-//	else {for (Ped m : Scheduler.allPeds) {
-//			if (m.yLoc < thisY) {
-//				ahead.add(m);}}}
-//	if (!ahead.isEmpty()) {
-//		for (Ped n : ahead) {
-//			if (n.dir == myDir) {
-//				leaders.add(n);}}
-//		if(!leaders.isEmpty()) {
-//			for (Ped o : leaders) {
-//				if(Math.abs(o.yLoc - thisY) < head) {
-//					head = Math.abs(o.yLoc - thisY);
-//					leader = o;}}}}
-//	
-//	//rest of code is not adjusted from car following
-//	if (leader != null) {
-//		setSpeed = leader.v;
-//		double vDiff = setSpeed - this.v;
-//		//double check the variables here - is setSpeed supposed to be used again?
-//		double safeHead = (jamHead + this.v*(this.tGap/.18) +
-//				((this.v*vDiff)/(2*Math.sqrt(this.maxa*this.mina))));
-//		a = this.maxa*(1 - Math.pow(this.v/this.maxv,4) - Math.pow(safeHead/head,2));}
-//	else {a = maxa*(1 - Math.pow(this.v/this.maxv,4));}
-//	return v;
-//}
